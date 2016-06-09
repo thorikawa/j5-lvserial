a8a3dc9f0bc4cff0a5ba07026e8af64eacc6f1c4
Takahiro Poly Horikawa <horikawa.takahiro@gmail.com>
2016-06-09 18:55:11 +0900

    Initial commit ... still not working

---
+++ example/example1.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let five = require('johnny-five');
+let LvSerial = require('../')(five);
+let board = new five.Board();
+
+board.on('ready', () => {
+	console.log('ready');
+	let vservo = new LvSerial({
+		pins: {
+			rx: 11,
+			tx: 10
+		}
+	});
+	setTimeout(() => {
+		vservo.unlock();
+	}, 200);
+	setTimeout(() => {
+		vservo.motorOn();
+	}, 400);
+	setTimeout(() => {
+		vservo.move(0, 0x500);
+	}, 2000);
+	setTimeout(() => {
+		vservo.move(0, 0xa00);
+	}, 4000);
+});
---
+++ src/index.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var Emitter = require("events").EventEmitter;
+var util = require("util");
+var priv = new Map();
+
+export default function(five) {
+	return (function() {
+		function Component(opts) {
+			if (!(this instanceof Component)) {
+				return new Component(opts);
+			}
+
+			five.Board.Component.call(
+				this, opts = five.Board.Options(opts)
+			);
+
+
+			var state = {
+				portId: opts.portId || this.io.SERIAL_PORT_IDs.DEFAULT,
+				baud: opts.baud || 115200,
+			};
+
+			priv.set(this, state);
+
+			let rx = 0, tx = 1;
+			if (this.pins) {
+				if (Number.isInteger(this.pins.rx)) rx = this.pins.rx;
+				if (Number.isInteger(this.pins.tx)) tx = this.pins.tx;
+			}
+			this.io.serialConfig({
+				portId: state.portId, 
+				baud: state.baud,
+				rxPin: rx,
+				txPin: tx,
+			});
+
+			this.io.serialRead(state.portId, (bytes) => {
+				// read bytes and determine event types to emit
+				// store received things in state object, use
+				// for emitting events 
+				// 
+				console.log(bytes);
+			});
+
+			Object.defineProperties(this, {
+				// Define any accessors here
+			});
+		}
+
+		util.inherits(Component, Emitter);
+
+		Component.prototype.write = function(bytes) {
+			let state = priv.get(this);
+			console.log(state.portId, bytes);
+			this.io.serialWrite(state.portId, bytes);
+		};
+
+		Component.prototype.flashWrite = function(sid, address, data) {
+			let len = data.length;
+			let buf = new Buffer(3 + len);
+			buf[0] = 0x80 | sid;
+			buf[1] = 0x40 | 0x00 | len;
+			buf[2] = address;
+			let offset = 3;
+			for (let d of data) {
+				buf[offset++] = d;
+			}
+			this.write(buf);
+		};
+
+		Component.prototype.unlock = function(sid=0) {
+			this.flashWrite(sid, 0x14, [0x55]);
+		};
+
+		Component.prototype.motorToggle = function(sid=0, on) {
+			this.flashWrite(sid, 0x3b, [on ? 1 : 0]);
+		};
+
+		Component.prototype.motorOn = function(sid=0) {
+			this.motorToggle(sid, true);
+		};
+
+		Component.prototype.motorOff = function(sid=0) {
+			this.motorToggle(sid, false);
+		};
+
+		Component.prototype.move = function(sid=0, tpos) {
+			if (tpos < 0x200 || tpos > 0xe00) {
+				console.error('tpos value is out of range. Please specify the value from 0x200 to 0xe00.');
+				return;
+			}
+			this.flashWrite(sid, 0x30, [tpos & 0x7f, tpos >> 7]);
+		};
+
+		return Component;
+	}());
+};
+
+/**
+ *  To use the plugin in a program:
+ *
+ *  var five = require("johnny-five");
+ *  var Component = require("component")(five);
+ *
+ *
+ */
